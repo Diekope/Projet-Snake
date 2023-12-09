cbe98f615e58ae2f2d176de27060f4e41ee0c457
Val <ValQuiTravaille@macbook-air-2.home>
2023-12-09 17:35:59 -0300

    Les fonctions nécessaires à l'allongement et au suivit de la tête

--- menus_Val/Menu/Game/game.h
+++ menus_Val/Menu/Game/game.h
@@ -8,9 +8,14 @@
 
 // ===Pour_les_fenetres_de_jeux_des_maps===
 typedef struct {
-    int x;  // Largeur de la fenêtre
-    int y;  // Hauteur de la fenêtre
+    int x, y;   // Coordonnées du segment
+    char name[10]; // Nom du segment (ex: q1, q2, ...)
 } GridSquare;
+
+typedef struct {
+    GridSquare *segments; // Tableau dynamique de segments
+    int length;           // Longueur actuelle du serpent
+} Snake;
 
 // ===Fenêtre_de_jeux===
 SDL_Window* CreateWindow(const char* title, int width, int height);
@@ -20,5 +25,17 @@ int gameWindow(int lg, int ht);
 void initializeRandom();
 int randCo(int x);
 void updateBonusPosition(GridSquare *bonus, int gridWidth, int gridHeight, int gridSize);
+
+// ===Snake===
+GridSquare* createSnake(int initialCapacity);
+// ===Agrandissement===
+GridSquare* resizeSnake(GridSquare* list, int newCapacity);
+// ===Ajouter_d'un_élément===
+void addToSnake(GridSquare* list, int index, int x, int y, const char* name);
+
+// ===Libéreration===
+void freeSnake(GridSquare* list);
+
+void createBodyPart(GridSquare** list, int* capacity, int* currentSize, int x, int y);
 
 #endif // GAME_H
