ada7a78db09c492ec6fd046d5312f2f2ad2fbc28
Val <ValQuiTravaille@MacBook-Air-2.local>
2023-12-18 14:32:06 -0300

    Partie Terminal

---
+++ Snake-Terminal/header/liste.h
@@ -0,0 +1,39 @@
+#ifndef LISTE_H_INCLUDED
+
+#define LISTE_H_INCLUDED
+
+
+
+#include "noeud.h"
+
+
+
+typedef struct serpent_ {
+
+    Noeud *sentAvt;
+
+    Noeud *sentArr;
+
+} serpent;
+
+
+
+serpent* serpentvide();
+
+void libererserpent(serpent *l);
+
+int estserpentvide(serpent *l);
+
+Noeud *iemeNoeud(serpent *l,int i);
+
+int iemeElt(serpent *l,int i);
+
+int serpentlongueur(serpent* l);
+
+void inserer(serpent *l,int i, int val,int x,int y);
+
+
+
+void afficherserpent(serpent *l);
+
+#endif //LISTE_H_INCLUDED
---
+++ Snake-Terminal/header/noeud.h
@@ -0,0 +1,53 @@
+#ifndef NOEUD_H_INCLUDED
+
+#define NOEUD_H_INCLUDED
+
+#include <stdio.h>
+
+#include <stdlib.h>
+
+
+
+
+
+typedef struct noeud Noeud;
+
+struct noeud{
+
+	int val;
+
+	int x;
+
+	int y;
+
+	int sent;
+
+	Noeud *suiv;
+
+};
+
+Noeud* creerNoeud(int val,int x,int y, Noeud *n);
+
+void libererNoeud(Noeud *n);
+
+Noeud *creerSent();
+
+int estSent(Noeud *n);
+
+int aSuivant(Noeud *n);
+
+Noeud* suivant(Noeud *n);
+
+int contenu(Noeud *n);
+
+int valeurx(Noeud *n);
+
+int valeurY(Noeud *n);
+
+void changercoord(Noeud* n,int x,int y);
+
+void changerCont(Noeud *n, int val,int x,int y);
+
+void changerSuiv(Noeud *n,Noeud *p);
+
+#endif //NOEUD_H_INCLUDED
